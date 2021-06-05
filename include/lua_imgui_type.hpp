ba6cc4bf87b8a6902e1275446355fcbf278902df
Kuanlan <Kuanlan@outlook.com>
2021-06-05 17:40:38 +0800

    可配置库名

--- include/lua_imgui_type.hpp
+++ include/lua_imgui_type.hpp
@@ -2,29 +2,31 @@
 #include "imgui.h"
 #include "lua.hpp"
 
-constexpr auto lua_class_imgui_ImVec2 = "imgui.ImVec2";
+constexpr const char lua_module_imgui[] = "imgui";
+
+constexpr const char lua_class_imgui_ImVec2[] = "imgui.ImVec2";
 
 void imgui_binding_lua_register_ImVec2(lua_State* L);
 ImVec2* imgui_binding_lua_new_ImVec2(lua_State* L);
 ImVec2* imgui_binding_lua_ref_ImVec2(lua_State* L, ImVec2* v);
 ImVec2* imgui_binding_lua_to_ImVec2(lua_State* L, int idx);
 
-constexpr auto lua_class_imgui_ImVec4 = "imgui.ImVec4";
+constexpr const char lua_class_imgui_ImVec4[] = "imgui.ImVec4";
 
 void imgui_binding_lua_register_ImVec4(lua_State* L);
 ImVec4* imgui_binding_lua_new_ImVec4(lua_State* L);
 ImVec4* imgui_binding_lua_ref_ImVec4(lua_State* L, ImVec4* v);
 ImVec4* imgui_binding_lua_to_ImVec4(lua_State* L, int idx);
 
-constexpr auto lua_class_imgui_ImGuiStyle = "imgui.ImGuiStyle";
+constexpr const char lua_class_imgui_ImGuiStyle[] = "imgui.ImGuiStyle";
 
 void imgui_binding_lua_register_ImGuiStyle(lua_State* L);
 ImGuiStyle* imgui_binding_lua_new_ImGuiStyle(lua_State* L);
 ImGuiStyle* imgui_binding_lua_ref_ImGuiStyle(lua_State* L, ImGuiStyle* v);
 ImGuiStyle* imgui_binding_lua_to_ImGuiStyle(lua_State* L, int idx);
 
-constexpr auto lua_class_imgui_string = "imgui.string";
-constexpr auto lua_class_imgui_ImGuiTextBuffer = "imgui.ImGuiTextBuffer";
+constexpr const char lua_class_imgui_string[] = "imgui.string";
+constexpr const char lua_class_imgui_ImGuiTextBuffer[] = "imgui.ImGuiTextBuffer";
 
 void imgui_binding_lua_register_ImGuiTextBuffer(lua_State* L);
 ImGuiTextBuffer* imgui_binding_lua_new_ImGuiTextBuffer(lua_State* L);
