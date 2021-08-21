2b3944c9bc460d11be273b419dc9fac87ce2581f
Kuanlan <Kuanlan@outlook.com>
2021-08-21 20:33:51 +0800

    杂七杂八的文件，去除VS项目（VS20XX屎一样的CMake支持）

---
+++ src/lua_imgui_map.cpp
@@ -0,0 +1,6 @@
+#include "lua_imgui_map.hpp"
+
+lua_imgui_Properties lua_imgui_MapProperties(const char* key, size_t len)
+{
+	return lua_imgui_Properties::__NOT_FOUND__;
+}
---
+++ src/lua_imgui_map.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+enum class lua_imgui_Properties
+{
+	__NOT_FOUND__,
+};
+
+lua_imgui_Properties lua_imgui_MapProperties(const char* key, size_t len);
